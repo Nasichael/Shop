5bc7eaee8973b937759822fb64ef31744eecdfcf
nasichael <agnieszka.halas@gmail.com>
2018-09-24 22:10:33 +0200

    Shop 24Sep18 - LatestVersion

--- src/main/java/com/example/demo/domain/Repository.java
+++ src/main/java/com/example/demo/domain/Repository.java
@@ -1,24 +1,4 @@
 package com.example.demo.domain;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class Repository {
-
-    JdbcTemplate jdbcTemplate;
-
-    public Repository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Product> search(String j) {
-     //   final List<Map<String, Object>> maps = jdbcTemplate.queryForList("");
-        final List<Product> products1 = jdbcTemplate.queryForList("", Product.class);
-
-        return new ArrayList<>();
-    }
 }
